aad5961ddd5727c4974545799557f630d31aa391
lukas <lukaskinvana@gmail.com>
2025-04-10 20:29:11 +0300

    Remove comments from code

--- broken_images/broken_images.py
+++ broken_images/broken_images.py
@@ -6,16 +6,13 @@ from urllib.parse import urljoin
 
 class TestBrokenImages(unittest.TestCase):
     def setUp(self):
-        """Set up the test environment before each test"""
         self.driver = webdriver.Chrome()
         self.driver.get("https://the-internet.herokuapp.com/broken_images")
         
     def tearDown(self):
-        """Clean up after each test"""
         self.driver.quit()
         
     def test_broken_images_count(self):
-        """Test that there are broken images on the page"""
         images = self.driver.find_elements(By.TAG_NAME, "img")
         broken_images = 0
         
@@ -30,18 +27,15 @@ class TestBrokenImages(unittest.TestCase):
         print(f"Total images checked: {len(images)}")
         print(f"Number of broken images: {broken_images}")
         
-        # Assert that there are broken images
         self.assertGreater(broken_images, 0, "No broken images found, but the page should have some")
         
     def test_all_images_loaded(self):
-        """Test that all images are loaded (status code 200)"""
         images = self.driver.find_elements(By.TAG_NAME, "img")
         
         for image in images:
             img_url = image.get_attribute("src")
             response = requests.get(img_url)
             
-            # Assert that each image returns a 200 status code
             self.assertEqual(response.status_code, 200, 
                            f"Image {img_url} returned status code {response.status_code}")
 
